5d6474dd586188e57e68dd6021451739933eeec5
MingMing <greeteaz567@gmail.com>
2022-05-29 19:44:19 -0500

    Finish header and line

---
+++ src/components/Line.js
@@ -0,0 +1,9 @@
+export default function Line (){
+    return (
+        <div className="line">
+            <div className="outerCircle">
+                <div className="innerCircle"></div>
+            </div>
+        </div>
+    )
+}
